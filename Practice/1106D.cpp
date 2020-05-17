6e66a7d5ad89babaf6f209b13e6cb48ed0f14263
Aditya <aditya.sharma5645@gmail.com>
2020-05-17 12:46:05 +0530

    Codeforces 1106D solution

--- Practice/1106D.cpp
+++ Practice/1106D.cpp
@@ -5,6 +5,42 @@ using namespace std;
 vector<ll> graph[100001];
 vector<bool> visited(100001, false);
 
+void priority_bfs(ll start)
+{
+
+  priority_queue<ll, vector<ll> , greater<int> > pq;
+  pq.push(start);
+  // set<ll> sett;
+  // sett.insert(start);
+  visited[start] = true;
+
+  // cout << "^^^ " << sett.size();
+
+  while(pq.size()!=0)
+  {
+    // auto i =  sett.begin();
+    // auto value = *i;
+    // sett.erase(sett.begin());
+
+    ll value = pq.top();
+    pq.pop();
+    cout << value << " ";
+
+
+    for(auto j: graph[value])
+    {
+      if(!visited[j])
+        pq.push(j);
+
+        visited[j] = true;
+    }
+
+
+  }
+  return;
+}
+
+
 void dfs(ll start)
 {
   cout << start << " ";
@@ -51,10 +87,10 @@ int main()
 //
 // cout << "\n" << "\n";
 
-  for(ll i = 0; i<=nodes ; i++)
-  {
-    sort(graph[i].begin(), graph[i].end());
-  }
+  // for(ll i = 0; i<=nodes ; i++)
+  // {
+  //   sort(graph[i].begin(), graph[i].end());
+  // }
 
   // for(ll i = 1; i<=nodes; i++)
   //   {
@@ -66,7 +102,9 @@ int main()
   //     cout << "\n";
   //   }
 
-  dfs(1);
+
+  // dfs(1);
+  priority_bfs(1);
   cout << "\n";
 
   return 0;
