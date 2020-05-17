3a6cf0a1f8279daf570a2430e52a740a82eb2d1b
Aditya <aditya.sharma5645@gmail.com>
2020-05-17 22:11:48 +0530

    Codeforces 1294C

--- Practice/1294C.cpp
+++ Practice/1294C.cpp
@@ -1,39 +1,39 @@
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
-
-ll smallestDivisor(ll a)
-{
-	if(a%2==0)
-		return 2;
-
-		for(ll i = 3; i*i<a; i+=3)
-		{
-			if(a%i==0)
-				return i;
-		}
-
-		return a;
-}
-
-
-ll largestDivisor(ll a)
-{
-	ll ans = 1;
-
-	if(a%2==0)
-	{
-			ans = 2;
-	}
-
-		for(ll i = 3; i*i<a; i+=3)
-		{
-			if(a%i==0)
-				ans = max(i, ans);
-		}
-
-		return ans;
-}
+//
+// ll smallestDivisor(ll a)
+// {
+// 	if(a%2==0)
+// 		return 2;
+//
+// 		for(ll i = 3; i*i<a; i+=3)
+// 		{
+// 			if(a%i==0)
+// 				return i;
+// 		}
+//
+// 		return a;
+// }
+//
+//
+// ll largestDivisor(ll a)
+// {
+// 	ll ans = 1;
+//
+// 	if(a%2==0)
+// 	{
+// 			ans = 2;
+// 	}
+//
+// 		for(ll i = 3; i*i<a; i+=3)
+// 		{
+// 			if(a%i==0)
+// 				ans = max(i, ans);
+// 		}
+//
+// 		return ans;
+// }
 
 
 int main()
@@ -51,52 +51,38 @@ int main()
     ll n;
     cin >> n;
 
-		bool ans = true;
-
 		ll a = 0, b = 0,c;
 
+		set<ll> divisors;
+
 		for(ll i = 2; i*i<=n; i++)
 		{
-				while(n%i==0)
+				if(n%i==0)
 				{
+					n/=i;
+					divisors.insert(i);
 					a = i;
-					n/=i;
-				}
-
-				if(!a)
 					break;
+				}
 		}
 
 		for(ll i = 2; i*i<=n; i++)
 		{
 
-				while(n%i==0)
+				if(n%i==0  && divisors.count(i) == 0)
 				{
-					b = i;
 					n/=i;
-				}
-
-				if(!b)
+					divisors.insert(i);
+					b = i;
 					break;
+				}
 		}
 
-		if(a == 0 || b == 0)
-			{
-				ans = false;
-			}
+			if(a!=0 && b!=0)
+				c = (n/(a*b));
 
-		else
-		c = n/(a*b);
-
-		// ll a = smallestDivisor(n);
-		// ll b = largestDivisor(n);
-		//
-
-		if(c == 1 || c == a || c == b)
-			ans = false;
-
-			if(!ans)
-			cout << "NO\n";
+			if(c == 1 || a == b || b == c || a == c)
+				cout << "NO\n";
 
 			else
 			cout << "YES\n" << a << " " << b << " " << c <<"\n";
