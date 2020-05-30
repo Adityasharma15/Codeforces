aae7679ac4cef8023ddb68277ba97414c64b36e4
Aditya <aditya.sharma5645@gmail.com>
2020-05-30 23:15:00 +0530

    Edu 88 C

---
+++ Edu 88 div2/C.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+  cin.tie(0);
+	cout.tie(0);
+
+	ll t;
+	cin >> t;
+	while(t--)
+  {
+
+    ll h, c , t;
+    cin >> h >> c >> t;
+
+    if(t >=h) cout << "1\n";
+
+    else
+    {
+
+
+
+
+
+
+    }
+
+	}
+
+  return 0;
+}
