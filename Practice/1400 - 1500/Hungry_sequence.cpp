1e6d3f8fb080b0cb5054dd90a95a694d41d67482
Aditya <aditya.sharma5645@gmail.com>
2020-11-20 17:39:39 +0530

    Hungry Sequence

---
+++ Practice/1400 - 1500/Hungry_sequence.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+  cin.tie(0);
+	cout.tie(0);
+
+  ll n;
+  cin >> n;
+
+  ll num = 100001;
+
+  while(n--){
+    cout << num << " ";
+    num++;
+  }
+
+  cout << "\n";
+  return 0;
+}
